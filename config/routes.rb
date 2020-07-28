090b8aaaa07f11e9bc692ce1573cd18cb88086bb
Christopher Chaison <cchaison0@gmail.com>
2020-07-28 14:53:22 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  root 'welcome#home'
+  get 'welcome/home', to: 'welcome#home'
   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  root 'welcome#home'
 end
